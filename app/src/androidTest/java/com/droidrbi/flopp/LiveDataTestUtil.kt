9773446d1d0a9554ee770441e230883585eb9335
ipshitaRB <ipshitarburman@gmail.com>
2020-07-10 19:01:49 -0400

    Add DAO tests for LiveData

---
+++ app/src/androidTest/java/com/droidrbi/flopp/LiveDataTestUtil.kt
@@ -0,0 +1,88 @@
+package com.droidrbi.flopp
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+
+
+/*@VisibleForTesting(otherwise = VisibleForTesting.NONE)
+fun <T> LiveData<T>.getOrAwaitValue(
+        time: Long = 2,
+        timeUnit: TimeUnit = TimeUnit.SECONDS,
+        afterObserve: () -> Unit = {}
+): T {
+    var data: T? = null
+    val latch = CountDownLatch(1)
+    val observer = object : Observer<T> {
+        override fun onChanged(o: T?) {
+            data = o
+            latch.countDown()
+            this@getOrAwaitValue.removeObserver(this)
+        }
+    }
+    this.observeForever(observer)
+
+    try {
+        afterObserve.invoke()
+
+        // Don't wait indefinitely if the LiveData is not set.
+        if (!latch.await(time, timeUnit)) {
+            throw TimeoutException("LiveData value was never set.")
+        }
+
+    } finally {
+        this.removeObserver(observer)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    return data as T
+}*/
+
+/**
+ * Gets the value of a [LiveData] or waits for it to have one, with a timeout.
+ *
+ * Use this extension from host-side (JVM) tests. It's recommended to use it alongside
+ * `InstantTaskExecutorRule` or a similar mechanism to execute tasks synchronously.
+ */
+fun <T> LiveData<T>.getOrAwaitValue(
+    time: Long = 2,
+    timeUnit: TimeUnit = TimeUnit.SECONDS,
+    afterObserve: () -> Unit = {}
+): T {
+    var data: T? = null
+    val latch = CountDownLatch(1)
+    val observer = object : Observer<T> {
+        override fun onChanged(o: T?) {
+            data = o
+            latch.countDown()
+            this@getOrAwaitValue.removeObserver(this)
+        }
+    }
+    this.observeForever(observer)
+
+    afterObserve.invoke()
+
+    // Don't wait indefinitely if the LiveData is not set.
+    if (!latch.await(time, timeUnit)) {
+        this.removeObserver(observer)
+        throw TimeoutException("LiveData value was never set.")
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    return data as T
+}
+
+@Throws(InterruptedException::class)
+fun <T> LiveData<T>.getValueBlocking(): T? {
+    var value: T? = null
+    val latch = CountDownLatch(1)
+    val innerObserver = Observer<T> {
+        value = it
+        latch.countDown()
+    }
+    observeForever(innerObserver)
+    latch.await(2, TimeUnit.SECONDS)
+    return value
+}
--- app/src/androidTest/java/com/droidrbi/flopp/MovieDatabaseTest.kt
+++ app/src/androidTest/java/com/droidrbi/flopp/MovieDatabaseTest.kt
@@ -1,5 +1,6 @@
 package com.droidrbi.flopp
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
@@ -7,18 +8,25 @@ import com.droidrbi.flopp.database.MovieDatabase
 import com.droidrbi.flopp.database.MovieDatabaseDao
 import com.droidrbi.flopp.database.model.Movie
 import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
-import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.IOException
 
+
 @RunWith(AndroidJUnit4::class)
 class MovieDatabaseTest {
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
 
     private lateinit var movieDao: MovieDatabaseDao
     private lateinit var db: MovieDatabase
+
 
     @Before
     fun createDb() {
@@ -39,7 +47,37 @@ class MovieDatabaseTest {
     }
 
     @Test
-    fun insertAndGetMovies() = runBlocking {
+    fun insertPopularMoviesSavesData() = runBlocking {
+        val movie1 = Movie(
+            id = 125,
+            overview = "overview",
+            posterUrl = "",
+            releaseDateString = "2014-12-23",
+            title = "Flubber",
+            voteAverage = 3.5,
+            voteCount = 5
+        )
+        val movie2 = Movie(
+            id = 100,
+            overview = "overview2",
+            posterUrl = "asdfaf",
+            releaseDateString = "2014-12-23",
+            title = "Flubber2",
+            voteAverage = 3.5,
+            voteCount = 5
+        )
+
+        val cachedMovies = listOf(movie1, movie2)
+
+        movieDao.insertPopularMovies(cachedMovies)
+
+        val retrievedMovies = movieDao.getAllMovies().getOrAwaitValue()
+
+        assertThat(retrievedMovies, `is`(not(emptyList())))
+    }
+
+    @Test
+    fun insertPopularMovies_listOfTwoMovies_listWithSameMovies() = runBlocking {
 
         val movie1 = Movie(
             id = 125,
@@ -60,30 +98,35 @@ class MovieDatabaseTest {
             voteCount = 5
         )
 
-        val movieList = listOf(movie1, movie2)
+        val cachedMovies = listOf(movie1, movie2)
 
-        movieDao.insertPopularMovies(movieList)
+        movieDao.insertPopularMovies(cachedMovies)
 
-        val insertedMovieList = movieDao.getAllMovies()
+        val retrievedMovies = movieDao.getAllMovies().getOrAwaitValue()
 
-        assertEquals(
-            "Inserted movie list does not match the one retrieved",
-            movieList[0].id,
-            insertedMovieList[0].id
-        )
+        // assert list size is same
+        assertThat(retrievedMovies.size, `is`(cachedMovies.size))
 
-
+        // assert list content and order is same
+        assertThat(retrievedMovies[0].title, `is`(cachedMovies[0].title))
+        assertThat(retrievedMovies[0].releaseDateString, `is`(cachedMovies[0].releaseDateString))
+        assertThat(retrievedMovies[0].posterUrl, `is`(cachedMovies[0].posterUrl))
+        assertThat(retrievedMovies[0].overview, `is`(cachedMovies[0].overview))
+        assertThat(retrievedMovies[0].id, `is`(cachedMovies[0].id))
+        assertThat(retrievedMovies[0].voteAverage, `is`(cachedMovies[0].voteAverage))
+        assertThat(retrievedMovies[0].voteCount, `is`(cachedMovies[0].voteCount))
+        assertThat(retrievedMovies[1].title, `is`(cachedMovies[1].title))
+        assertThat(retrievedMovies[1].releaseDateString, `is`(cachedMovies[1].releaseDateString))
+        assertThat(retrievedMovies[1].posterUrl, `is`(cachedMovies[1].posterUrl))
+        assertThat(retrievedMovies[1].overview, `is`(cachedMovies[1].overview))
+        assertThat(retrievedMovies[1].id, `is`(cachedMovies[1].id))
+        assertThat(retrievedMovies[1].voteAverage, `is`(cachedMovies[1].voteAverage))
+        assertThat(retrievedMovies[1].voteCount, `is`(cachedMovies[1].voteCount))
     }
 
     @Test
-    fun emptyDatabaseTest() = runBlocking {
-        val emptyMovieList = movieDao.getAllMovies()
-        assertEquals("List is not empty", 0, emptyMovieList.size)
+    fun insertPopularMovies_nothing_emptyList() = runBlocking {
+        val emptyCachedMovies = movieDao.getAllMovies().getOrAwaitValue()
+        assertThat(emptyCachedMovies, `is`(emptyList()))
     }
-
-    @Test
-    fun compareListTest() {
-        // compare without using dbId
-    }
-
 }
